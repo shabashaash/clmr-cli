680f774d6ec2ec8ce6ad9eb6a05a50f766e5a24f
shabashaash <shabasha.ura@yandex.ru>
2022-09-18 03:43:05 +0300

    _ = SimCLR(encoder, args.projection_dim, (n_features))

--- classifier_script.py
+++ classifier_script.py
@@ -74,7 +74,7 @@ def main(args):
 
     state_dict = load_encoder_checkpoint(args.checkpoint_path)
     encoder.load_state_dict(state_dict)
-    # encoder = SimCLR(encoder, args.projection_dim, (n_features))
+    _ = SimCLR(encoder, args.projection_dim, (n_features))
     
 
 
